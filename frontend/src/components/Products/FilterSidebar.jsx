d0d340d9d5f0256310f036381447bd99c0f4e958
LeThiMyKieu <kieuqt98@gmail.com>
2025-06-13 10:04:28 +0700

    filter, urlParams, handleFilterChange...in Search

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 function FilterSidebar() {
-    const [searchParams, setSearchParams] = useSearchParams();
+    /**
+     * searchParams: Dùng để đọc giá trị tham số truy vấn
+     * setSearchParams: Dùng để cập nhật query params trên URL
+     */
+    const [searchParams, setSearchParams] = useSearchParams();  
+
+    const navigation = useNavigate();
     // x.com/?a=1&b=2
+    // khai báo state, lưu trữ bộ lọc
     const [filters, setFilters] = useState({
         category: "",
         gender: "",
@@ -17,7 +24,6 @@ function FilterSidebar() {
     const [priceRange, setPriceRange] = useState([0, 100]);
 
     const categories = ["Top Wear", "Bottom Wear"];
-
     const colors = [
         "Red",
         "Blue",
@@ -30,9 +36,7 @@ function FilterSidebar() {
         "Beige",
         "Navy",
     ];
-
     const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
-
     const materials = [
         "Cotton",
         "Wool",
@@ -43,7 +47,6 @@ function FilterSidebar() {
         "Viscose",
         "Fleece",
     ];
-
     const brands = [
         "Urban Threads",
         "Modern Fit",
@@ -54,16 +57,16 @@ function FilterSidebar() {
     ];
     const genders = ["Men", "Women"];
 
+    // đồng bộ các tham số URL (searchParams) với state lọc (filters)
     useEffect(() => {
-        const params = Object.fromEntries([...searchParams]);
-        // {category: "Top Wear", maxPrice: 100} => params.category
-
-        setFilters({
+        const params = Object.fromEntries([...searchParams]); // chuyển các query params thành object JS >>Ví dụ: URL: ...?category=Top Wear&size=M,L → params = { category: "Top Wear", size: "M,L" }
+        //cập nhật state filters dựa trên các giá trị từ URL.
+        setFilters({ 
             category: params.category || "",
             gender: params.gender || "",
             color: params.color || "",
             size: params.size ? params.size.split(",") : [],
-            material: params.material ? params.material.split(",") : [],
+            material: params.material ? params.material.split(",") : [], 
             brand: params.brand ? params.brand.split(",") : [],
             minPrice: params.minPrice || 0,
             maxPrice: params.maxPrice || 100,
@@ -73,12 +76,12 @@ function FilterSidebar() {
 
     const handleFilterChange = (e) => {
         const { name, value, checked, type } = e.target;
-        // console.log({name, value, checked, type});
+
         let newFilters = { ...filters };
 
         if (type === "checkbox") {
             if (checked) {
-                newFilters[name] = [...FilterSidebar(newFilters[name] || []), value];
+                newFilters[name] = [...(newFilters[name] || []), value];
             }
             else {
                 newFilters[name] = newFilters[name].filters((item) => item !== value);
@@ -89,9 +92,35 @@ function FilterSidebar() {
             newFilters[name] = value;
         };
         setFilters(newFilters);
-        console.log(newFilters);
-        
-    }
+        updateURLParams(newFilters);
+    };
+
+    const updateURLParams = (newFilters) => {
+        const params = new URLSearchParams();
+        Object.keys(newFilters).forEach((key) => {
+            if (Array.isArray(newFilters[key]) && newFilters[key].length > 0) {
+                params.append(key, newFilters[key].join(",")); // "XS,S"
+            } else if (newFilters[key]) {
+                params.append(key, newFilters[key]);
+            }
+        });
+        setSearchParams(params);
+        navigation(`?${params.toString()}`); // ?category=Bottom+Wear&size=XS%2CS
+    };
+
+    const handlePriceChange = (e) => {
+        const newPrice = e.target.value;
+        setPriceRange([0, newPrice]);
+
+        const newFilters = {
+            ...filters,
+            minPrice: 0,
+            maxPrice: newPrice
+        };
+
+        setFilters(newFilters);
+        updateURLParams(newFilters); // Cập nhật URL với filters mới
+    };
     return (
         <div className="p-4">
             <h3 className="text-xl font-medium text-gray-800 mb-4">Filter</h3>
@@ -107,6 +136,7 @@ function FilterSidebar() {
                                     name="category"
                                     value={category}
                                     onChange={handleFilterChange}
+                                    checked={filters.category === category}
                                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
                                 />
                                 <span className="text-gray-700">{category}</span>
@@ -124,9 +154,10 @@ function FilterSidebar() {
                             <div key={gender} className="flex items-center mb-1">
                                 <input
                                     type="radio"
-                                    name="category"
+                                    name="gender"
                                     value={gender}
                                     onChange={handleFilterChange}
+                                    checked={filters.gender === gender}
                                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
                                 />
                                 <span className="text-gray-700">{gender}</span>
@@ -144,8 +175,8 @@ function FilterSidebar() {
                             return (
                                 <button key={color} name="color"
                                     value={color}
-                                    onChange={handleFilterChange}
-                                    className="w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105"
+                                    onClick={handleFilterChange}
+                                    className={`w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105 ${filters.color === color ? "right-2 ring-blue-500" : ""}`}
                                     style={{ backgroundColor: color.toLowerCase() }}>
                                 </button>
                             )
@@ -165,6 +196,7 @@ function FilterSidebar() {
                                     name="size"
                                     value={size}
                                     onChange={handleFilterChange}
+                                    checked={filters.size.includes(size)}
                                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300" />
                                 <span className="text-gray-700">{size}</span>
                             </div>
@@ -183,6 +215,7 @@ function FilterSidebar() {
                                     name="material"
                                     value={material}
                                     onChange={handleFilterChange}
+                                    checked={filters.material.includes(material)}
                                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300" />
                                 <span className="text-gray-700">{material}</span>
                             </div>
@@ -201,6 +234,7 @@ function FilterSidebar() {
                                     name="brand"
                                     value={brand}
                                     onChange={handleFilterChange}
+                                    checked={filters.brand.includes(brand)}
                                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300" />
                                 <span className="text-gray-700">{brand}</span>
                             </div>
@@ -216,11 +250,13 @@ function FilterSidebar() {
                     name="priceRanger"
                     min={0}
                     max={100}
+                    value={priceRange[1]}
+                    onChange={handlePriceChange}
                     className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
                 />
                 <div className="flex justify-between text-gray-600 mt-2">
                     <span>$0</span>
-                    <span>$100</span>
+                    <span>${priceRange[1]}</span>
                 </div>
             </div>
         </div>
