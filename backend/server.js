45bd8140215a7fda5b18810f9bdd9888aac1f989
LeThiMyKieu <kieuqt98@gmail.com>
2025-06-16 16:03:59 +0700

    setup backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// connect to MongoDB
+connectDB();
+
+app.get("/", (req,res) => {
+    res.send("Welcome to rabbit api!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Severs is running on http://localhost:${PORT}`);
+})
