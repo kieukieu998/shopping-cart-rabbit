0fe1acd7232661975e793ed8e5be9ec61ce0d08a
LeThiMyKieu <kieuqt98@gmail.com>
2025-06-17 11:20:50 +0700

    auth user

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const e = require("express");
+
+//Middleware to protect routes
+
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1]; // Lấy token từ header
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Giải mã token
+
+      req.user = await User.findById(decoded.user.id).select("-password"); //Exclude password
+      next();
+    } catch (error) {
+        console.error("Token verification failed", error);
+
+        res.status(401).json({message: "Not authorized, token failed"});
+    }
+  } else {
+    res.status(401).json({message: "Not authorizes, no token provided"})
+  }
+};
+
+module.exports = protect;
