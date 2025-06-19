7363d7a83f9c30a4b463aea3354757a07319510f
LeThiMyKieu <kieuqt98@gmail.com>
2025-06-19 16:38:03 +0700

    cartSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReduce from "./slices/productsSlice"
+import cartReduce from "./slices/cartSlice"
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    products: productReduce
+    products: productReduce,
+    cart: cartReduce
   },
 });
 
